2be06b1e90a966895dd6c00ca7d4c8b3a84b8762
Marisa Robayo Moreno <marisarobayo@gmail.com>
2024-09-18 13:26:22 +0200

    Ignore folders on a separate config. (#261)

--- src/index.js
+++ src/index.js
@@ -2,85 +2,94 @@ import ts from "typescript-eslint";
 import globals from "globals";
 import js from "@eslint/js";
 
-export default ts.config(js.configs.recommended, ...ts.configs.recommended, {
-  files: ["**/*.ts", "**/*.tsx"],
-  plugins: {
-    "@typescript-eslint": ts.plugin,
-  },
-  ignores: ["dist/", "tmp/", "src/env.d.ts"],
-  languageOptions: {
-    globals: {
-      ...globals.browser,
-      ...globals.node,
+export default ts.config(
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": ts.plugin,
     },
-    parser: ts.parser,
-    ecmaVersion: 11,
-    sourceType: "module",
-    parserOptions: {
-      project: ["./tsconfig.json"],
+    ignores: ["dist/", "tmp/", "src/env.d.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: ts.parser,
+      ecmaVersion: 11,
+      sourceType: "module",
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
     },
-  },
-  rules: {
-    indent: "off",
-    "@typescript-eslint/indent": ["off"],
-    "linebreak-style": ["error", "unix"],
-    "eol-last": "error",
-    "key-spacing": "error",
-    curly: "error",
-    "space-before-blocks": "error",
-    "space-infix-ops": "error",
-    "spaced-comment": ["error", "always"],
-    "function-paren-newline": ["error", "consistent"],
-    "arrow-parens": "off",
-    "space-in-parens": ["error", "never"],
-    "no-trailing-spaces": [
-      "error",
-      {
-        skipBlankLines: true,
-      },
-    ],
-    "array-bracket-spacing": ["error", "never"],
-    semi: ["error", "never"],
-    "comma-dangle": "off",
-    "@typescript-eslint/comma-dangle": "off",
-    "dot-notation": "off",
-    "@typescript-eslint/dot-notation": "error",
-    "no-undef": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        varsIgnorePattern: "_",
-        argsIgnorePattern: "_",
-      },
-    ],
-    "prefer-const": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unnecessary-type-assertion": "error",
-    "no-var": "error",
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/await-thenable": "error",
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        checksVoidReturn: {
-          arguments: false,
+    rules: {
+      indent: "off",
+      "@typescript-eslint/indent": ["off"],
+      "linebreak-style": ["error", "unix"],
+      "eol-last": "error",
+      "key-spacing": "error",
+      curly: "error",
+      "space-before-blocks": "error",
+      "space-infix-ops": "error",
+      "spaced-comment": ["error", "always"],
+      "function-paren-newline": ["error", "consistent"],
+      "arrow-parens": "off",
+      "space-in-parens": ["error", "never"],
+      "no-trailing-spaces": [
+        "error",
+        {
+          skipBlankLines: true,
         },
-      },
-    ],
-    "no-confusing-arrow": [
-      "error",
-      {
-        allowParens: false,
-      },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        name: "lodash",
-        message: "Please use lodash-es instead.",
-      },
-    ],
+      ],
+      "array-bracket-spacing": ["error", "never"],
+      semi: ["error", "never"],
+      "comma-dangle": "off",
+      "@typescript-eslint/comma-dangle": "off",
+      "dot-notation": "off",
+      "@typescript-eslint/dot-notation": "error",
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "_",
+          argsIgnorePattern: "_",
+        },
+      ],
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unnecessary-type-assertion": "error",
+      "no-var": "error",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            arguments: false,
+          },
+        },
+      ],
+      "no-confusing-arrow": [
+        "error",
+        {
+          allowParens: false,
+        },
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          name: "lodash",
+          message: "Please use lodash-es instead.",
+        },
+      ],
+    },
   },
-});
+  {
+    // These ignores are on a separate config object so ESLint will consider them global
+    // On ESLint 9.x folders are omitted on non global ignores
+    ignores: ["dist/", "tmp/", "src/env.d.ts"],
+  }
+);
